9570531a60d5c13cd74ee092dd4e8c9422ca9045
Christy Fury <christy.fury@gmail.com>
2023-04-09 11:16:31 -0400

    connects partials and adds exercises to seed route

--- db/seed.js
+++ db/seed.js
@@ -3,38 +3,79 @@ const premadeExercises = [
         name: "bicep curl", 
         description: "lebow flexion",
         tags: ["arms", "biceps"],
-        img: "https://media4.giphy.com/media/t6cV5EFu66TqE/giphy.gif?cid=ecf05e47dmg1x27tei5y5gvqvl8ywuar58zbot2e1i9xnd28&rid=giphy.gif&ct=g",
+        img: "https://media4.giphy.com/media/t6cV5EFu66TqE/giphy.gif?cid=ecf05e47dmg1x27tei5y5gvqvl8ywuar58zbot2e1i9xnd28&rid=giphy.gif&ct=g"
     },
     {
         name: "leg extension", 
         description: "kick leg straight",
         tags: ["quads", "quad", "legs", "lower-body"],
-        img: String,
-        },
+        img: String
+    },
     {
         name: "crunch", 
         description: "classic crunch",
         tags: ["core", "abs"],
-        img: String,
-        },
+        img: String
+    },
     {
         name: "calf riase", 
         description: "lift heels",
         tags: ["ankles", "calves", "lower-body"],
-        img: String,
-        },
+        img: String
+    },
     {
         name: "tricep extension ", 
         description: " extend elbow",
         tags: ["arms", "triceps", "upper-body"],
-        img: String,
-        },
+        img: String
+    },
     {
         name: "sled push", 
         description: "load up some weight and push",
         tags: ["legs", "glutes", "sled"],
-        img: "https://thumbs.gfycat.com/ImpressionableSkeletalCranefly-mobile.mp4",
-        },
+        img: "https://post.healthline.com/wp-content/uploads/2020/12/Female_Sled_Push_732x549-thumbnail-1.jpg"
+    },
+    {
+        name: "oherhead press ", 
+        description: "press overhead",
+        tags: ["arms", "shoulders","deltoids", "upper-body"],
+        img: String
+    },
+    {
+        name: "lateral raise", 
+        description: "arms bent, rasie up",
+        tags: ["arms", "deltoids", "shoulders", "upper-body"],
+        img: String
+    },
+    {
+        name: "goblet squat", 
+        description: "hold weight at chest height, squat low",
+        tags: ["legs", "glutes", "lower-body", "quads"],
+        img: String
+    },
+    { 
+        name: "Split Squat", 
+        description: "rear leg up on an elevates surface, other does the work",
+        tags: ["legs", "quads", "glutes", "lower-body"],
+        img: String
+    }, 
+    {
+        name: "Supine leg lowers", 
+        description: "lay on back, keep legs straight, lift and lower",
+        tags: ["core", "abs"],
+        img: String,
+    },
+    {
+        name: "Bent over row", 
+        description: "bend at hips, pull weight up next to waist",
+        tags: ["back", "shoulders", "upper-body", "arms"],
+        img: String,
+    }
 ]
 
 module.exports = premadeExercises;
+
+// name: "", 
+// description: "",
+// tags: ["", "", "", ""],
+// img: String,
