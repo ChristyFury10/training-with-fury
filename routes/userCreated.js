f686afbcb9cfc34189f6327fe16a9e06e7e10eb5
Christy Fury <christy.fury@gmail.com>
2023-04-10 22:42:39 -0400

    adds bootstrap navbar and links home and categories

--- routes/userCreated.js
+++ routes/userCreated.js
@@ -17,7 +17,16 @@ router.get("/created", async (req, res)=>{
     res.render("userCreated.ejs", {userExercises})
 })
 
+router.get("/:id/edit", async (req, res)=>{
+    const exercise = await UserExercise.findById(req.params.id)
+    res.render("edit.ejs", {exercise})
+});
 
+router.put("/:id", async (req, res)=>{
+    const id = req.params.id;
+    const exercise = await UserExercise.findByIdAndUpdate(id, req.body, {new:true});
+    res.redirect("./created")
+});
 
 router.delete("/:id", async (req, res)=>{
     const exercise = await UserExercise.findByIdAndDelete(req.params.id);
