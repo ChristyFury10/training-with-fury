fb099094e4242283dc24e411328055f2bde15704
Christy Fury <christy.fury@gmail.com>
2023-09-17 21:33:41 -0400

    adds url to connection file

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 // require("dotenv").config();
 
 //connect mongoose woth mongoDB url
-mongoose.connect(process.env.DATABASE_URL);
+// mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect('mongodb+srv://project:4KjGdOWhaAQWN520@cluster0.qs3kxed.mongodb.net/exercisesDB?retryWrites=true&w=majority', {useNewUrlParser: true});
 
 // export mongoose so that models can interface with it
 module.exports= {mongoose};
